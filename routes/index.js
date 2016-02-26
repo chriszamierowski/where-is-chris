11f1d411eff391677a4c1be8d6656d48259ffc23
Chris Zamierowski <chriszamierowski@gmail.com>
2016-02-26 00:02:00 -0800

    some math for figuring out what dates to display

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,36 @@ var router = express.Router();
 var google = require('googleapis');
 var request = require('request');
 
+var monthsByName = {
+  january: 0,
+  february: 1,
+  march: 2,
+  april: 3,
+  may: 4,
+  june: 5,
+  july: 6,
+  august: 7,
+  september: 8,
+  october: 9,
+  november: 10,
+  december: 11
+};
+
+var monthsByNumber = {
+  0 : 'january',
+  1 : 'february',
+  2 : 'march',
+  3 : 'april',
+  4 : 'may',
+  5 : 'june',
+  6 : 'july',
+  7 : 'august',
+  8 : 'september',
+  9 : 'october',
+  10 : 'november',
+  11 : 'december'
+};
+
 function getEvents(callback) {
   request({
     url:'https://www.googleapis.com/calendar/v3/calendars/'+process.env.GOOGLE_CALENDAR_ID+'/events?key='+process.env.GOOGLE_CALENDAR_API_KEY,
@@ -14,16 +44,77 @@ function getEvents(callback) {
   });
 }
 
-/* GET home page. */
+function daysInMonth(month,year) {
+  return new Date(year, month + 1, 0).getDate();
+}
 
-router.get('/', function(req, res) {
+function handleRequest(req, res) {
+  var currentDate = new Date(),
+      currentMonth = currentDate.getMonth(),
+      currentYear = currentDate.getFullYear(),
+      month = req.params.month,
+      englishMonth,
+      year = currentYear;
+
+  //get a month from url, otherwise use current
+  if(month) {
+    if(monthsByName[month] === undefined) {
+      res.redirect('/');
+      return;
+    } else {
+      month = monthsByName[month];
+    }
+  } else {
+    month = currentMonth;
+  }
+
+  //get the year of the next time that month occurs
+  if(currentMonth > month) {
+    year++;
+  }
+
+  englishMonth = monthsByNumber[month];
+
+  console.log('month:',month);
+    console.log('year:',year);
+
   getEvents(function(body){
     console.log('body.items',body.items);
     res.render('index', {
       title: 'Express',
-      items: body.items
+      items: body.items,
+      englishMonth: englishMonth,
+      prevMonth: findPrevMonth(month, currentMonth),
+      nextMonth: findNextMonth(month, currentMonth),
+      year: year,
+      days: daysInMonth(month, year)
     });
   });
-});
+}
+
+function findPrevMonth(month, currentMonth) {
+  //don't show past months
+  if(month === currentMonth) {
+    return null;
+  } else {
+    return monthsByNumber[Math.abs(month + 11) % 12];
+  }
+}
+
+function findNextMonth(month, currentMonth) {
+  //don't show more than a year ahead
+  var next = Math.abs(month + 1) % 12;
+
+  if(next === currentMonth) {
+    return null;
+  } else {
+    return monthsByNumber[next];
+  }
+}
+/* GET home page. */
+
+router.get('/', handleRequest);
+
+router.get('/:month', handleRequest);
 
 module.exports = router;
