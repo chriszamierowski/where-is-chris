f46e9bdb70a1c9433a3c49ab2c585a53d3a2b9c0
Chris Zamierowski <chriszamierowski@gmail.com>
2016-02-26 18:48:31 -0800

    display events

---
+++ README.md
@@ -0,0 +1,2 @@
+doesn't support multiple events on one date
+only supports all-day events
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var google = require('googleapis');
 var request = require('request');
+var _ = require('lodash');
 
 var monthsByName = {
   january: 0,
@@ -34,11 +35,13 @@ var monthsByNumber = {
 };
 
 function getEvents(callback) {
+  console.log('https://www.googleapis.com/calendar/v3/calendars/'+process.env.GOOGLE_CALENDAR_ID+'/events?key='+process.env.GOOGLE_CALENDAR_API_KEY+'&location=34,-118');
   request({
-    url:'https://www.googleapis.com/calendar/v3/calendars/'+process.env.GOOGLE_CALENDAR_ID+'/events?key='+process.env.GOOGLE_CALENDAR_API_KEY,
+    url:'https://www.googleapis.com/calendar/v3/calendars/'+process.env.GOOGLE_CALENDAR_ID+'/events?key='+process.env.GOOGLE_CALENDAR_API_KEY+'&location=34,-118',
     json: true
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      console.log(body);
       callback(body);
     }
   });
@@ -68,7 +71,9 @@ function formatMonth(year, month, daysInMonth) {
     if(dCounter < firstDay) {
       days[0][dCounter%daysInWeek] = null;
     } else {
-      days[w][dCounter%daysInWeek] = d;
+      days[w][dCounter%daysInWeek] = {
+        date: new Date(year, month, d)
+      };
       d++;
     }
 
@@ -109,14 +114,15 @@ function handleRequest(req, res) {
   daysInMonth = getDaysInMonth(year, month);
   formattedMonth = formatMonth(year, month, daysInMonth);
 
-  getEvents(function(body){
+  getEvents(function (body){
+
+    formatEvents(body.items, year, month, formattedMonth);
+
     res.render('index', {
       title: 'Express',
-      items: body.items,
-      englishMonth: englishMonth,
+      heading: englishMonth+' '+year,
       prevMonth: findPrevMonth(month, currentMonth),
       nextMonth: findNextMonth(month, currentMonth),
-      year: year,
       formattedMonth: formattedMonth
     });
   });
@@ -139,6 +145,68 @@ function findNextMonth(month, currentMonth) {
     return null;
   } else {
     return monthsByNumber[next];
+  }
+}
+
+function formatEvents(items, year, month, formattedMonth) {
+  var events = [],
+      e,
+      event,
+      eventCal = [],
+      i,
+      w,
+      d,
+      sDay,
+      tempStart,
+      monthStart = new Date(year, month, 1),
+      monthEnd = new Date(year, month+1, 0, 23, 59, 59),
+      duration;
+
+  // simplify gcal obj
+  for(i in items) {
+    //manipulate the way google stores dates, slightly
+    tempStart = new Date(items[i].start.date)
+    tempStart.setDate(tempStart.getDate() + 1, 0, 0, 0);
+
+    events.push({
+      name: items[i].summary,
+      start: tempStart,
+      end: new Date(items[i].end.date)
+    });
+  }
+
+  // triple for loops with a while inside seems efficient!
+  for(e in events) {
+    event = events[e];
+
+    //(event starts this month) || (event ends this month)
+    if((monthStart <= event.start && monthEnd >= event.start) || (monthStart <= event.end && monthEnd >= event.end)) {
+      for(w = 0; w < formattedMonth.length; w++) {
+        for(d = 0; d < formattedMonth[w].length; d++) {
+          // (event already account for && date exists) && ((event start before date || event day before day because of gcal weirdness with what timezone it returns))
+          if((!event.addedToCal && formattedMonth[w][d]) && ((event.start <= formattedMonth[w][d].date) || (event.start.getDate() <= formattedMonth[w][d].date.getDate()))) {
+            //how long does this event last?
+            duration = (event.end - (event.start > monthStart ? event.start : monthStart))/(1000*60*60*24) + 1;
+            event.addedToCal = true;
+            event.duration = duration;
+            formattedMonth[w][d].event = _.clone(event);
+            
+            //check for wrapping events
+            while(formattedMonth[w] && duration > (formattedMonth[w].length-d)) {
+              formattedMonth[w][d].event.duration = formattedMonth[w].length-d;
+              duration = duration - (formattedMonth[w].length-d);
+
+              if(formattedMonth[w+1]) {
+                w++;
+                d=0;
+                event.duration = duration;
+                formattedMonth[w][0].event = _.clone(event);
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
